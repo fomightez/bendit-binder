b9d459e6b17d8fd099ec7d36d485a4c8ecdfb595
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2020-02-24 17:34:18 -0500

    make the list into a string

--- .github/actions/make_lw_nb/main.py
+++ .github/actions/make_lw_nb/main.py
@@ -1,7 +1,7 @@
 import os
 with open("index.ipynb", 'r') as index_nb:
     lines = index_nb.readlines()
-lines = "".join()
+lines = "".join(lines)
 lines = lines.replace('"lightweight_archive = False"','"lightweight_archive = True"')
 with open("lw.ipynb", 'w') as output:
     output.write(lines)
