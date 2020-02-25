f57da8c134bbec5ce892977775801dff580972c5
fomightez <fomightez@gmail.com>
2020-02-24 21:24:07 -0500

    move python to a script

--- .github/actions/make_lw_nb/main.py
+++ .github/actions/make_lw_nb/main.py
@@ -1,7 +1,6 @@
 import os
 with open("index.ipynb", 'r') as index_nb:
-    lines = index_nb.readlines()
-lines = "".join(lines)
-lines = lines.replace('"lightweight_archive = False"','"lightweight_archive = True"')
+    conent = index_nb.read()
+content = conent.replace('"lightweight_archive = False"','"lightweight_archive = True"')
 with open("lw.ipynb", 'w') as output:
-    output.write(lines)
+    output.write(content)
