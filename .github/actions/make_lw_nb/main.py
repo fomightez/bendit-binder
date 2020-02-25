b730dda39c3b6b1f67d8a0b81ce4e542bed5747a
fomightez <wayne4.antispam@server.fake>
2020-02-25 13:48:57 -0500

    streamline python script use

--- .github/actions/make_lw_nb/main.py
+++
@@ -1,6 +0,0 @@
-import os
-with open("index.ipynb", 'r') as index_nb:
-    conent = index_nb.read()
-content = conent.replace('"lightweight_archive = False"','"lightweight_archive = True"')
-with open("lw.ipynb", 'w') as output:
-    output.write(content)
