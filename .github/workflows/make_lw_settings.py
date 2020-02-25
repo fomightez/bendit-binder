ebdf595db5ec682b40ed3fc25555c1b93ae952c5
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2020-02-25 14:08:58 -0500

    update to write out as index nb
    update to write out as index nb because now making index nb with settings in different branch, specifically the lw branch

--- .github/workflows/make_lw_settings.py
+++ .github/workflows/make_lw_settings.py
@@ -1,6 +1,8 @@
 import os
-with open("index.ipynb", 'r') as index_nb:
+nb_to_change_settings_in = "index.ipynb"
+with open(nb_to_change_settings_in, 'r') as index_nb:
     conent = index_nb.read()
-content = conent.replace('"lightweight_archive = False"','"lightweight_archive = True"')
-with open("lw.ipynb", 'w') as output:
+content = conent.replace(
+    '"lightweight_archive = False"','"lightweight_archive = True"')
+with open(nb_to_change_settings_in, 'w') as output:
     output.write(content)
