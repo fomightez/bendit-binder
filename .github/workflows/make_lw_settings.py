b9d5ff04524496a59eceea6d918a1ff89a5e7188
fomightez <wayne4.antispam@server.fake>
2020-03-06 17:11:11 -0500

    add cleaning step toggle

--- .github/workflows/make_lw_settings.py
+++ .github/workflows/make_lw_settings.py
@@ -1,9 +1,11 @@
 import os
 nb_to_change_settings_in = "index.ipynb"
 with open(nb_to_change_settings_in, 'r') as index_nb:
-    conent = index_nb.read()
-content = conent.replace(
+    content = index_nb.read()
+content = content.replace(
     '"lightweight_archive = False"','"lightweight_archive = True"')
+content = content.replace(
+    '"cleaning_step = True"','"cleaning_step = False"')
 with open(nb_to_change_settings_in, 'w') as output:
     output.write(content)
     
